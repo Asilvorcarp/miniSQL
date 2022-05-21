d2d249bcf82d05560d2aa07b28b65333c3316b11
xp-dong <1457992388@qq.com>
2022-05-21 22:58:56 +0800

    disabled my_b_plus_tree_test

--- minisql/test/index/MY_b_plus_tree_test.cpp
+++ minisql/test/index/MY_b_plus_tree_test.cpp
@@ -7,7 +7,7 @@
 
 static const std::string db_name = "bp_tree_insert_test.db";
 
-TEST(BPlusTreeTests, SampleTest) {
+TEST(BPlusTreeTests, DISABLED_SampleTest) {
   // Init engine
   DBStorageEngine engine(db_name);
   BasicComparator<int> comparator;
@@ -29,7 +29,7 @@ TEST(BPlusTreeTests, SampleTest) {
     delete_seq.push_back(i);
   }
 
-  Shuffle data
+  //Shuffle data
   ShuffleArray(keys);
   ShuffleArray(values);
   ShuffleArray(delete_seq);
