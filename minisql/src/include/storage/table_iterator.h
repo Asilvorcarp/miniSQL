504c7db995e3cf099d393ed39ad112c97ae11754
Asilvorcarp <asilvorcarp@qq.com>
2022-05-27 13:04:25 +0800

    fix: error if inline

--- minisql/src/include/storage/table_iterator.h
+++ minisql/src/include/storage/table_iterator.h
@@ -26,9 +26,9 @@ public:
 
   virtual ~TableIterator();
 
-  inline bool operator==(const TableIterator &itr) const;
+  bool operator==(const TableIterator &itr) const;
 
-  inline bool operator!=(const TableIterator &itr) const;
+  bool operator!=(const TableIterator &itr) const;
 
   const Row &operator*();
 
