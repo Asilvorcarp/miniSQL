169098be27b44f44d10dec45f479135343a35a38
Asilvorcarp <asilvorcarp@qq.com>
2022-06-09 21:05:46 +0800

    chore: disable debug outputs

--- minisql/src/main.cpp
+++ minisql/src/main.cpp
@@ -12,7 +12,7 @@ FILE *yyin;
 #include "parser/parser.h"
 }
 
-#define ENABLE_PARSER_DEBUG // debug
+// #define ENABLE_PARSER_DEBUG // debug
 // #define ENABLE_FILE_INPUT // debug, read cmds from file, only support one line 
 // #define ENABLE_FILE_OUTPUT // debug, output to "output.txt"
 
